c3686c82021d5c7c3611035929f99cca571a5403
david <mail@nonswag.dev>
2026-03-22 13:47:17 +0100

    Rename fields in ProGuard mapping payload for consistency

--- packages/proguard-plugin/src/main/kotlin/dev/faststats/proguard/SourcemapPayload.kt
+++ packages/proguard-plugin/src/main/kotlin/dev/faststats/proguard/SourcemapPayload.kt
@@ -2,13 +2,12 @@ package dev.faststats.proguard
 
 data class SourcemapUpload(
     val fileName: String,
-    val sourcemap: String,
+    val mapping: String,
 )
 
 data class SourcemapUploadPayload(
     val buildId: String,
     val mappingType: String,
-    val mapping: String,
     val uploadedAt: String,
-    val sourcemaps: List<SourcemapUpload>,
+    val mappings: List<SourcemapUpload>,
 )
