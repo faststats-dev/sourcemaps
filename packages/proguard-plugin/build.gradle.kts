1ab954f3dc9e62718983d1364373b4bc58de4bb3
david <mail@nonswag.dev>
2026-03-22 13:31:01 +0100

    Add initial implementation of the ProGuard plugin

---
+++ packages/proguard-plugin/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    kotlin("jvm") version "2.3.20"
+    id("java-gradle-plugin")
+    id("maven-publish")
+}
+
+group = "dev.faststats"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+    google()
+}
+
+dependencies {
+    implementation("com.google.code.gson:gson:2.13.1")
+    compileOnly("com.android.tools.build:gradle:8.7.3")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+kotlin {
+    jvmToolchain(11)
+}
+
+gradlePlugin {
+    plugins {
+        create("proguardMappingsUpload") {
+            id = "dev.faststats.proguard-mappings-upload"
+            implementationClass = "dev.faststats.proguard.FastStatsProguardPlugin"
+            displayName = "Faststats ProGuard Mapping Upload Plugin"
+            description = "Uploads ProGuard/R8 obfuscation mapping files to the FastStats sourcemaps API"
+        }
+    }
+}
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        // pom.url.set("https://docs.faststats.dev/serviceio")
+        pom.scm {
+            val repository = "FastStats-dev/sourcemaps"
+            url.set("https://github.com/$repository/tree/main/packages/proguard-plugin")
+            connection.set("scm:git:git://github.com/$repository.git")
+            developerConnection.set("scm:git:ssh://github.com/$repository.git")
+        }
+        from(components["java"])
+    }
+    repositories.maven {
+        val branch = if (version.toString().contains("-pre")) "snapshots" else "releases"
+        url = uri("https://repo.thenextlvl.net/$branch")
+        credentials {
+            username = System.getenv("REPOSITORY_USER")
+            password = System.getenv("REPOSITORY_TOKEN")
+        }
+    }
+}
