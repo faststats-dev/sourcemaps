7acb4ce7865e4609663bc0999cf1910d5a71e598
Luca <lucameopou@gmail.com>
2026-06-28 12:18:31 +0200

    chore: format

--- packages/bundler-plugin/tests/plugin.test.ts
+++ packages/bundler-plugin/tests/plugin.test.ts
@@ -7,7 +7,7 @@ import { type MultiStats, rspack, type Stats } from "@rspack/core";
 import { build as esbuildBuild } from "esbuild";
 import type { NormalizedOutputOptions, OutputBundle } from "rollup";
 import { rollup as createRollupBundle } from "rollup";
-import { build as viteBuild, type InlineConfig } from "vite";
+import { type InlineConfig, build as viteBuild } from "vite";
 import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import webpack from "webpack";
 import bunPlugin from "../src/bundler/bun";
