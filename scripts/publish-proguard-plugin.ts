669651243eb4e6e83906ab0e2634a4c9bf35819f
Luca <lucameopou@gmail.com>
2026-06-28 11:53:42 +0200

    chore: format

--- scripts/publish-proguard-plugin.ts
+++ scripts/publish-proguard-plugin.ts
@@ -33,16 +33,12 @@ if (head.ok) {
 	process.exit(0);
 }
 
-const result = spawnSync(
-	gradlew,
-	["publishAllPublicationsToMavenRepository"],
-	{
-		cwd: pluginDir,
-		stdio: "inherit",
-		env: process.env,
-		shell: false,
-	},
-);
+const result = spawnSync(gradlew, ["publishAllPublicationsToMavenRepository"], {
+	cwd: pluginDir,
+	stdio: "inherit",
+	env: process.env,
+	shell: false,
+});
 
 if (result.status !== 0) {
 	process.exit(result.status ?? 1);
